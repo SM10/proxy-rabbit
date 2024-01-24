89d5858240dc449aab9a27e2aa11e2ef56fa445c
Stephen Man <stephenman88@gmail.com>
2024-01-24 04:40:48 -0500

    styled and implemented cards

--- src/components/FindByProduct/FindByProduct.js
+++ src/components/FindByProduct/FindByProduct.js
@@ -1,5 +1,9 @@
 import "./FindByProduct.scss"
 import { useState } from "react"
+import ProductCard from "../ProductCard/ProductCard"
+import mascot from "../../assets/images/proxy-rabbit-mascot.png"
+import {Slide} from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 
 function FindByProduct(){
 
@@ -12,7 +16,7 @@ function FindByProduct(){
     const onSubmit = (e) => {
         e.preventDefault();
     }
-
+    //2 cards on mobile, 3 on tablet
     return (<main className="findbyproduct main">
         <section className="findbyproduct-find">
             <div className="findbyproduct-find-head">
@@ -27,27 +31,37 @@ function FindByProduct(){
                 <hr className="findbyproduct-find-head__line--bottom" />
             </div>
             <div className="findbyproduct-find-body">
-
+                <Slide className="findbyproduct-find-body-slide"> 
+                    <div className="findbyproduct-find-body-slide-batch">
+                    <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />   
+                    <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />
+                    <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />
+                    </div>
+                </Slide>
             </div>
         </section>
         <section className="findbyproduct-popular">
             <div className="findbyproduct-popular-head">
                 <hr className="findbyproduct-popular-head__line--top" />
-                <h2 className="findbyproduct-popular-head__title">Most Popular</h2>
+                <div className="findbyproduct-popular-head-content">
+                    <h2 className="findbyproduct-popular-head__title">Most Popular</h2>
+                </div>
                 <hr className="findbyproduct-popular-head__line--bottom" />
             </div>
             <div className="findbyproduct-popular-body">
-
+                <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />
             </div>
         </section>
         <section className="findbyproduct-discovery">
             <div className="findbyproduct-discovery-head">
                 <hr className="findbyproduct-discovery-head__line--top" />
-                <h2 className="findbyproduct-discovery-head__title">Discover These Products</h2>
+                <div className="findbyproduct-discovery-head-content">
+                    <h2 className="findbyproduct-discovery-head__title">Discover These Products</h2>
+                </div>
                 <hr className="findbyproduct-discovery-head__line--bottom" />
             </div>
             <div className="findbyproduct-discovery-body">
-
+                <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />
             </div>
         </section>
     </main>)

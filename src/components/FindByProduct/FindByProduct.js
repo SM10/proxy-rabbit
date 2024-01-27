db8d10f1c15f29c31c17ce1c0a70f731f4411ebd
Stephen Man <stephenman88@gmail.com>
2024-01-27 16:02:27 -0500

    fixed findbyproducts

--- src/components/FindByProduct/FindByProduct.js
+++ src/components/FindByProduct/FindByProduct.js
@@ -1,13 +1,117 @@
 import "./FindByProduct.scss"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ProductCard from "../ProductCard/ProductCard"
 import mascot from "../../assets/images/proxy-rabbit-mascot.png"
 import {Slide} from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
+import axios from "axios";
 
 function FindByProduct(){
 
     const [searchInput, setSearchInput] = useState('')
+    const [searchedProducts, setSearchedProducts] = useState([]);
+    const [searchedProductsHtml, setSearchedProductsHtml] = useState([]);
+    const [popularProducts, setPopularProducts] = useState([]);
+    const [popularProductsHtml, setPopularProductsHtml] = useState([]);
+    const [discoverProducts, setDiscoverProducts] = useState([]);
+    const [discoverProductsHtml, setDiscoverProductsHtml] = useState([]);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(()=>{
+        let productCardBatches = [];
+            let counter = 0;
+            while(counter < discoverProducts.length){
+                productCardBatches.push((<div className="findbyproduct-find-body-slide-batch">
+                    {(()=>{
+                        const productArray = []
+                        let innerCounter = 0
+                        while(counter < discoverProducts.length && innerCounter < 3){
+                            productArray.push(<ProductCard product={{image: discoverProducts[counter].image_url, name:discoverProducts[counter].name, description: discoverProducts[counter].country_name}} />   )
+                            innerCounter++
+                            counter++
+                        }
+                        return productArray
+                    })()}
+                </div>))
+            }
+            setDiscoverProductsHtml(productCardBatches)
+    }, discoverProducts)
+
+    useEffect(()=>{
+        let productCardBatches = [];
+        let counter = 0;
+        while(counter < popularProducts.length){
+            productCardBatches.push((<div className="findbyproduct-find-body-slide-batch">
+                {(()=>{
+                    const productArray = []
+                    let innerCounter = 0
+                    while(counter < popularProducts.length && innerCounter < 3){
+                        productArray.push(<ProductCard product={{image: popularProducts[counter].image_url, name:popularProducts[counter].name, description: popularProducts[counter].country_name}} />   )
+                        innerCounter++
+                        counter++
+                    }
+                    return productArray
+                })()}
+            </div>))
+        }
+        setPopularProductsHtml(productCardBatches)
+    }, [popularProducts])
+
+    useEffect(()=>{
+            let productCardBatches = [];
+            let counter = 0;
+            while(counter < searchedProducts.length){
+                productCardBatches.push((<div className="findbyproduct-find-body-slide-batch">
+                    {(()=>{
+                        const productArray = []
+                        let innerCounter = 0
+                        while(counter < searchedProducts.length && innerCounter < 3){
+                            productArray.push(<ProductCard product={{image: searchedProducts[counter].image_url, name:searchedProducts[counter].name, description: searchedProducts[counter].country_name}} />   )
+                            innerCounter++
+                            counter++
+                        }
+                        return productArray
+                    })()}
+                </div>))
+            }
+            setSearchedProductsHtml(productCardBatches)
+    }, [searchedProducts])
+
+    useEffect(()=>{
+        (async ()=>{
+            try{
+                const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/products`)
+                setSearchedProducts(response.data);
+            }catch(error){
+                console.log(error)
+            }
+        })();
+        (async ()=>{
+            try{
+                const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/products/popular`)
+                setPopularProducts(response.data);
+            }catch(error){
+                console.log(error)
+            }
+        })();
+        (async ()=>{
+            try{
+                const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/products/discover`)
+                setDiscoverProducts(response.data);
+            }catch(error){
+                console.log(error)
+            }
+        })();
+        
+
+        function onWindowResize(){
+            setScreenWidth(window.innerWidth);
+        }
+
+        setScreenWidth(window.innerWidth);
+        window.addEventListener("resize", onWindowResize)
+        return () => window.removeEventListener("resize", onWindowResize)
+    }, [])
 
     const onSearchChange = (e) => {
         setSearchInput(e.target.value)
@@ -16,6 +120,7 @@ function FindByProduct(){
     const onSubmit = (e) => {
         e.preventDefault();
     }
+
     //2 cards on mobile, 3 on tablet
     return (<main className="findbyproduct main">
         <section className="findbyproduct-find">
@@ -32,11 +137,12 @@ function FindByProduct(){
             </div>
             <div className="findbyproduct-find-body">
                 <Slide className="findbyproduct-find-body-slide"> 
-                    <div className="findbyproduct-find-body-slide-batch">
+                    {searchedProductsHtml}
+                    {/*<div className="findbyproduct-find-body-slide-batch">
                     <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />   
                     <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />
                     <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />
-                    </div>
+                    </div>*/}
                 </Slide>
             </div>
         </section>
@@ -49,7 +155,7 @@ function FindByProduct(){
                 <hr className="findbyproduct-popular-head__line--bottom" />
             </div>
             <div className="findbyproduct-popular-body">
-                <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />
+            <Slide className="findbyproduct-popular-body-slide">{popularProductsHtml} </Slide>
             </div>
         </section>
         <section className="findbyproduct-discovery">
@@ -61,7 +167,7 @@ function FindByProduct(){
                 <hr className="findbyproduct-discovery-head__line--bottom" />
             </div>
             <div className="findbyproduct-discovery-body">
-                <ProductCard product={{image: mascot, name:"Proxy Rabbit", description: "The website mascot"}} />
+            <Slide className="findbyproduct-discovery-body-slide">{discoverProductsHtml} </Slide>
             </div>
         </section>
     </main>)
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -3,7 +3,7 @@ import './ProductCard.scss';
 function ProductCard({product}){
     return(<div className='product-card'>
     <div className='product-card-container'>
-        <img className='product-card-container__image' src={product.image} alt={product.name}/>
+        <img className='product-card-container__image' src={`${product.image}`} alt={product.name}/>
         <div className='product-card-container-right'>
             <h3 className='product-card-container-right__name'>{product.name}</h3>
             {product.description ? <p className='product-card-container-right__description'>{product.description}</p> : ''}

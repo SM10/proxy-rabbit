3476550e2929c046c23eb3ef5893739b5e17d0f4
Stephen Man <stephenman88@gmail.com>
2024-01-27 23:25:49 -0500

    chat box appears now

--- src/App.js
+++ src/App.js
@@ -64,7 +64,6 @@ function App() {
         <Route path='/FindByLocation' element={<FindByLocation supportedCountries={countries} onCountryClicked={onCountryClicked}/>} />
         <Route path='/FindByProduct' element={<FindByProduct onProductCardClicked={getUsersByCountryId}/>} />
         <Route path='/Login' element={<Login setIsLoggedIn={setIsLoggedIn} setUserProfile={setUserProfile}/>} />
-        <Route path='/Logout' element={<Logout />} />
         <Route path='/Register' element={<Register />} />
         <Route path='/Mailbox' element={<Mailbox userProfile={userProfile}/>} />
         <Route path='*' element={<PageNotFound />} />

7bef6bfd76016315c93db48f9fef6d0db02a6589
Stephen Man <stephenman88@gmail.com>
2024-01-27 21:55:31 -0500

    set up credentials

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -1,11 +1,29 @@
 import './ChatList.scss';
 import UserCard from '../UserCard/UserCard';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function ChatList(){
+    const [chatList, setChatList] = useState([])
+
+    useEffect(()=>{
+        (async ()=>{
+            try{
+            const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/message`, {withCredentials: true})
+            console.log(response.data);
+            setChatList(response.data);
+            }catch(error){
+                
+            }
+        })()
+    }, [])
+
     return(
     <section className='chatlist'>
         <div className='chatlist-container'>
-            <UserCard user={{first_name: "Stephen", last_name: "Man"}} country ="Canada" />
+            {chatList.map(chatItem => {
+                return(<UserCard user={{first_name: chatItem.first_name, last_name: chatItem.last_name}} country ={chatItem.country_name} />)
+            })}
         </div>
     </section>)
 }

a7b33e61b705eb7c2a33133bdcae9603d9c8cd5f
Stephen Man <stephenman88@gmail.com>
2024-01-29 04:41:08 -0500

    send button works now

--- src/components/MessageUserPopup/MessageUserPopup.js
+++ src/components/MessageUserPopup/MessageUserPopup.js
@@ -5,31 +5,74 @@ import {useState} from "react";
 
 function MessageUserPopup({isLoggedIn, onCloseClicked, recipient}){
     const [sendInput, setSendInput] = useState('')
+    const [isMessageSent, setIsMessageSent] = useState(false);
+
+    const onSendButtonClicked = (e) =>{
+        e.preventDefault()
+            const sendObject = {
+                recipient_id: recipient.id,
+                message: e.target.send.value
+            };
+            console.log(sendObject);
+            (async ()=>{
+                try{
+                    const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/message`,sendObject, {withCredentials: true})
+                    setIsMessageSent(true);
+                }catch(error){
+                    console.log(error)
+                }
+            })();
+    }
+
+    const onTextInputPressed = (e)=>{
+        if(e.key === "Enter"){
+            e.preventDefault()
+            const sendObject = {
+                recipient_id: recipient.id,
+                message: e.target.send.value
+            };
+            console.log(sendObject);
+            (async ()=>{
+                try{
+                    const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/message`,sendObject, {withCredentials: true})
+                    setIsMessageSent(true);
+                }catch(error){
+                    console.log(error)
+                }
+            })();
+            setSendInput('')
+        }
+    }
 
     if(!isLoggedIn){
-        <div className='popup-background'>
+        return (<div className='popup-background'>
         <section className='message-popup'>
-            <button className='message-popup__exit' onClick={()=>{onCloseClicked()}}><img src={closeIcon} alt='close button icon' className='message-popup__exit__icon' /></button>
+            <button className='message-popup__exit' onClick={()=>{onCloseClicked(); setIsMessageSent(false)}}><img src={closeIcon} alt='close button icon' className='message-popup__exit__icon' /></button>
             <div className="message-popup-error">
                 <h1 className="message-popup-error_text">You must be logged in to message the user.</h1>
             </div>
         </section>
-    </div>
+    </div>)
     }
 
     return (<div className='popup-background'>
         <section className='message-popup'>
             <button className='message-popup__exit' onClick={()=>{onCloseClicked()}}><img src={closeIcon} alt='close button icon' className='message-popup__exit__icon' /></button>
+            {isMessageSent ? 
+            <div className="message-popup-error">
+                <h1 className="message-popup-error_text">Message sent.</h1>
+            </div> : 
             <div className="message-popup-head">
                 <h1 className="message-popup-head__title">Message {recipient.first_name}</h1>
-            </div>
-            <form className="message-popup-message">
-                <textarea className="message-popup-message__input" name="search" placeholder="Search" onChange={(e)=>{setSendInput(e.target.value)}} value={sendInput}/>
+            </div>}
+            {isMessageSent ? '' : <form className="message-popup-message" onSubmit={onSendButtonClicked}>
+                <textarea className="message-popup-message__input" name="send" placeholder="Enter a message" onKeyDown={onTextInputPressed} onChange={(e)=>{setSendInput(e.target.value)}} value={sendInput}/>
                 <div className='message-popup-message-buttons'>
                     <button className='message-popup-message-buttons__cancel' type='button' onClick={()=>{onCloseClicked()}}>Cancel</button>
                     <button className='message-popup-message-buttons__submit' type='submit'>Send</button>
                 </div>
-            </form>
+            </form>}
+            
         </section>
     </div>)
 }
